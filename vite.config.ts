3c5ed0a0ddf1f3e3b8281ce2502ec8d3f819b670
Dipanshu <dipanshumahato@gmail.com>
2025-02-01 14:34:27 +0530

    added preview
    Signed-off-by: Dipanshu <dipanshumahato@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/portfolio',
 });
